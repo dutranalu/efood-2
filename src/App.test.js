fb287d967b63f44abf87dcfe627d8dbbd9b2917d
analu <dutranalu@hotmail.com>
2026-01-29 12:22:18 -0300

    Fix test setup and router version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './styles/theme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <ThemeProvider theme={theme}>
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    </ThemeProvider>
+  );
+  const loadingText = screen.getByText(/carregando restaurantes/i);
+  expect(loadingText).toBeInTheDocument();
 });
